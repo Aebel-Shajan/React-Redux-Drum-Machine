0750d0a85bbb6f1d8e007adb092c6fa2e23d5a80
Aebel Shajan <aebelshajan@gmail.com>
2023-12-04 23:19:52 +0000

    Added the CSS for the drum machine. Created the different react components

--- src/App.js
+++ src/App.js
@@ -1,58 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const audioBaseUrl = "https://s3.amazonaws.com/freecodecamp/drums/"
+
+	const drumPads = [
+		{ key: "Q", audio: "Heater-1" },
+		{ key: "W", audio: "Heater-2" },
+		{ key: "E", audio: "Heater-3" },
+		{ key: "A", audio: "Heater-4_1" },
+		{ key: "S", audio: "Heater-6" },
+		{ key: "D", audio: "Dsc_Oh" },
+		{ key: "Z", audio: "Kick_n_Hat" },
+		{ key: "X", audio: "RP4_KICK_1" },
+		{ key: "C", audio: "Cev_H2" }
+	]
+
+	function displayPads() {
+		return drumPads.map(element => {
+			return (
+				<button type="button" className='drum-pad' id={element.audio} key={element.audio}>
+					<audio src={audioBaseUrl + element.audio + ".mp3"} className='clip' id={element.key}>
+					</audio>
+					{element.key}
+				</button>
+			)
+		})
+	}
+
+	return (
+		<div className="inner-container" id="drum-machine">
+			<div className="controls-container" id="controls">
+				<Toggle name="Power"/>
+				<Toggle name="Bank"/>
+				<div id="drum-display" ></div>
+				<input type="range" min="1" max="100" className="slider"/>
+			</div>
+			<div className="pad-container" id="pads">
+				{displayPads()}
+			</div>
+		</div>
+	);
 }
+
+function Toggle(props) {
+	return (
+		<div className="toggle-container">
+			{props.name}
+			<label className="switch">
+			<input type="checkbox" className="switch-input" />
+			<span className="toggle"></span>
+		</label>
+		</div>
+		
+	);
+};
+
+
 
 export default App;
